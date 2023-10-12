afad119c6862fdec0c739cf6a851053376f14f8a
Piotr Wójtowicz <piotrekwjtowicz@yahoo.pl>
2023-10-12 17:18:35 +0200

    oAuth testing

---
+++ oAuthTest/src/main/java/com/example/oauthtest/OAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthTestApplication.class, args);
+	}
+
+}
