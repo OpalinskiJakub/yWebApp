7ae91db234b74b58f9cfaf63e1ce11644d7885a3
Jakub Opalinski <jakopal1125@gmail.com>
2023-11-05 15:16:50 +0100

    add mongo and react

---
+++ ApiBackendYWebApp/src/main/java/opalinski/jakub/ApiBackendYWebApp/ApiBackendYWebAppApplication.java
@@ -0,0 +1,13 @@
+package opalinski.jakub.ApiBackendYWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBackendYWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBackendYWebAppApplication.class, args);
+	}
+
+}
