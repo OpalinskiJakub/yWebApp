1e84c3340a85b8a7c7ae57e7d50e9dd73bf3012d
Piotr Wójtowicz <piotrekwjtowicz@yahoo.pl>
2023-12-18 19:30:15 +0100

    small changes

--- ApiBackendYWebApp/src/main/java/opalinski/jakub/ApiBackendYWebApp/DemoEndpoint.java
+++ ApiBackendYWebApp/src/main/java/opalinski/jakub/ApiBackendYWebApp/DemoEndpoint.java
@@ -1,12 +1,14 @@
 package opalinski.jakub.ApiBackendYWebApp;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/tokenmang/demo")
+@PreAuthorize("hasRole('USER')")
 public class DemoEndpoint {
 
     @GetMapping
--- ApiBackendYWebApp/src/main/java/opalinski/jakub/ApiBackendYWebApp/config/JwtService.java
+++ ApiBackendYWebApp/src/main/java/opalinski/jakub/ApiBackendYWebApp/config/JwtService.java
@@ -22,8 +22,6 @@ public class JwtService {
   private String secretKey;
   @Value("${application.security.jwt.expiration}")
   private long jwtExpiration;
-  @Value("${application.security.jwt.refresh-token.expiration}")
-  private long refreshExpiration;
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
